b84e22888ff8e9e8f1483aaf9b05575fcf56208a
Gregor <gregorkuhelj3@gmail.com>
2023-10-15 13:00:54 +0200

    Dodal nalogo Two Sum

--- TwoSum.py
+++ TwoSum.py
@@ -1,10 +1,7 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-
-        for i in range(len(nums) + 1):
-            j = i + 1 # definiramo naslednjega
-            if nums[i] + nums[j] == target: 
-                return i,j
-            j += 1 # pogledamo naslednjega
-
-        
+            for i in range(len(nums)):
+                for j in range(i+1,len(nums)): # pogledamo naslednji element v seznamu
+                    if nums[i] + nums[j] == target:
+                        return i,j
+                    
